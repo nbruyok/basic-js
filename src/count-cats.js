ba68f7027f7d258ab31487dc993b50dd22a173c9
Nastya Bruyok <nastyabruyok@gmail.com>
2022-05-01 18:23:28 +0300

    feat: implement functions count-cats, drea-team, matrix-elements-sum

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let sum = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    //строка
+    for (let j = 0; j < matrix[0].length; j++) {
+      // колонна этой строки
+      if (matrix[i][j] === "^^") {
+        sum++;
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
