8af4569710e4fcf523ab712d8f350cb1ed4e86d8
Nastya Bruyok <nastyabruyok@gmail.com>
2023-04-17 22:50:26 +0300

    feat: implement function for dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+
+  for (let element of domains) {
+    const arr = element.split('.').reverse();
+    let domain = '';
+
+    arr.forEach((element) => {
+      domain += '.' + element;
+
+      result.hasOwnProperty(domain) ? result[domain]++ : (result[domain] = 1);
+    });
+  }
+
+  return result;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
